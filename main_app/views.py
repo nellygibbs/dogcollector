1923758fd5131bce2611d950183072eecb8482ab
nellygibbs <dgibs19@yahoo.com>
2022-03-05 14:24:06 -0500

    adds anchor tag to view page so user can click each dog

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-#fake database
-class Dog:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-#List of Dogs
-dogs = [
-    Dog('Jazzy', 'Yorkie', 'heaven sent', 5),
-    Dog('Blacky', 'Unknown', 'worst nightmare', 13),
-    Dog('Storm', 'Pit Bull', 'ball of energy', 1),
-]        
-
+from .models import Dog
+       
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -24,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})   
